4684f5395be7542af7f588ec0a3ff4c1ebf16a0d
LawNM <77174991+LawNM@users.noreply.github.com>
2021-02-09 13:09:24 -0700

    Add files via upload

---
+++ Week6/js/windchill.js
@@ -0,0 +1,8 @@
+
+                let x = parseFloat(document.getElementById("highTemp").innerHTML);
+                let y = parseFloat(document.getElementById("windSpeed").innerHTML);
+
+                let chillFactor = 35.74 + 0.6125 * x - 35.75 * Math.pow(y, 0.16) + 0.4275 * x * Math.pow(y, 0.16);
+                let printedOutput = chillFactor
+                
+                document.getElementById("output").textContent = "The wind chill factor is " + printedOutput.toFixed(1) + " °F";
