5334c58d0b4e0dfd97b39f41916ee61173c1d291
LawNM <77174991+LawNM@users.noreply.github.com>
2021-02-13 16:55:32 -0700

    Add files via upload

--- Week6/js/windchill.js
+++ Week6/js/windchill.js
@@ -4,5 +4,10 @@
 
                 let chillFactor = 35.74 + 0.6125 * x - 35.75 * Math.pow(y, 0.16) + 0.4275 * x * Math.pow(y, 0.16);
                 let printedOutput = chillFactor
-                
+                if(x > 50 || y <= 3){
+
+                }
+
+                else{
                 document.getElementById("output").textContent = "The wind chill factor is " + printedOutput.toFixed(1) + " °F";
+                }
