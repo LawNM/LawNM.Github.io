92a78f885369eb5a0300dbf867449d5143c7cf9e
LawNM <77174991+LawNM@users.noreply.github.com>
2021-03-13 15:47:20 -0700

    Add files via upload

--- week10/js/weatherReader.js
+++ week10/js/weatherReader.js
@@ -4,10 +4,10 @@ fetch (apiURL)
     .then((jsObject) => {
         console.log (jsObject);
 
-        document.getElementById("current-temp").textContent = " " + jsObject.main.temp + " °F";
-        document.getElementById("description").textContent = " " + jsObject.weather[0].description;
-        document.getElementById("humidity").textContent = " " + jsObject.main.humidity + "%";
-        document.getElementById("windSpeed").textContent = " " + jsObject.wind.speed + " MPH";
+        document.getElementById("current-temp").textContent = jsObject.main.temp + " °F";
+        document.getElementById("description").textContent = jsObject.weather[0].description;
+        document.getElementById("humidity").textContent = jsObject.main.humidity + "%";
+        document.getElementById("windSpeed").textContent = jsObject.wind.speed + " MPH";
     })
 
     const castURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=7fb334586cf67f2a93d9a12e96f99ad5"
@@ -21,6 +21,28 @@ fetch (castURL)
         document.getElementById("wed").textContent = newObject.list[21].main.temp;
         document.getElementById("thur").textContent = newObject.list[29].main.temp;
         document.getElementById("fri").textContent = newObject.list[37].main.temp;
-    })
 
-    document.getElementById("description").innerText.charAt(0).toUpperCase();
+        let days = new Array(7);
+        days[0] = "Sunday";
+        days[1] = "Monday";
+        days[2] = "Tuesday";
+        days[3] = "Wednesday";
+        days[4] = "Thursday";
+        days[5] = "Friday";
+        days[6] = "Saturday";
+        days[7] = "Sunday";
+        days[8] = "Monday";
+        days[9] = "Tuesday";
+        days[10] = "Wednesday";
+    
+        let day = new Date();
+        var d = day.getDay();
+        
+    
+        document.getElementById("dia1").textContent = days[d+1];
+        document.getElementById("dia2").textContent = days[d+2];
+        document.getElementById("dia3").textContent = days[d+3];
+        document.getElementById("dia4").textContent = days[d+4];
+        document.getElementById("dia5").textContent = days[d+5];
+
+    })
